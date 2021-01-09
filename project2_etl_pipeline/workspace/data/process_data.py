38b96132456d6c5d0974aa48cafca3b8834ec98e
venknat <venk.natarajan@gmail.com>
2021-01-09 15:17:03 -0800

    add docstrings

--- project2_etl_pipeline/workspace/data/process_data.py
+++ project2_etl_pipeline/workspace/data/process_data.py
@@ -3,15 +3,26 @@ import pandas as pd
 import sqlalchemy
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    Loads data from .csv files containing message contents and category labels
+    :param messages_filepath: file containing messages from figure 8
+    :param categories_filepath: file containing categories corresponding to these messages
+    :return: pandas dataframe merging data from these two sets together
+    """
     messages_df = pd.read_csv('disaster_messages.csv')
     categories_df = pd.read_csv('disaster_categories.csv')
     return messages_df.merge(categories_df, how='left', on='id').set_index('id')
 
 def clean_data(df):
+    """
+    Takes the "raw" data loaded from the csv files and returns a dataframe organized os that each category
+    is in its own column.
+    :param df: pandas dataframe consisting of data rawly loaded from db
+    :return: clean db so semi-colon separated categories are c
+    """
     categories_df = df.categories.str.split(';', expand=True)
     first_row = categories_df.iloc[0]
     colnames = first_row.map(lambda x: x.split('-')[0])
-    print(colnames.head())
     categories_df.columns = colnames
     for column in categories_df:
         categories_df[column] = categories_df[column].map(lambda x: x.split('-')[1]).astype(int)
@@ -19,12 +30,24 @@ def clean_data(df):
     return df.merge(categories_df, how='left', left_index=True, right_index=True)
 
 def save_data(df, database_filename, if_exists):
+    """
+    Saves df to the 'messages' table of the sqllite database at database_filename
+    :param df: pandas dataframe to save
+    :param database_filename: filename where the database is to be persisted
+    :param if_exists: passed to pandas to_sql in case database table already exists
+    :return: None
+    """
     table_name='messages'
     engine = sqlalchemy.create_engine('sqlite:///{}'.format(database_filename))
     df.to_sql('messages', engine, if_exists=if_exists, index=True)
 
 
 def main():
+    """
+    Runs the ETL pipeline to convert messages and categories csv files to a well-structured
+    sqlite db.
+    :return:
+    """
     if len(sys.argv) >= 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
