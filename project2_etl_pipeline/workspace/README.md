62802ecaafe893f6b2eec5a45c1c6d945ad43952
venknat <venk.natarajan@gmail.com>
2021-01-09 15:10:25 -0800

    Set up ETL pipeline

--- project2_etl_pipeline/workspace/README.md
+++ project2_etl_pipeline/workspace/README.md
@@ -4,7 +4,10 @@
 1. Run the following commands in the project's root directory to set up your database and model.
 
     - To run ETL pipeline that cleans data and stores in database
-        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`.
+      By default this will delete the existing `messages` table in the database file, to override this
+      an additional argument can be added that should match a valid value to the `if_exists`
+      parameter of `pandas.DataFrame.to_sql`
     - To run ML pipeline that trains classifier and saves
         `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
 
--- project2_etl_pipeline/workspace/data/process_data.py
+++ project2_etl_pipeline/workspace/data/process_data.py
@@ -1,20 +1,31 @@
 import sys
-
+import pandas as pd
+import sqlalchemy
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages_df = pd.read_csv('disaster_messages.csv')
+    categories_df = pd.read_csv('disaster_categories.csv')
+    return messages_df.merge(categories_df, how='left', on='id').set_index('id')
 
 def clean_data(df):
-    pass
+    categories_df = df.categories.str.split(';', expand=True)
+    first_row = categories_df.iloc[0]
+    colnames = first_row.map(lambda x: x.split('-')[0])
+    print(colnames.head())
+    categories_df.columns = colnames
+    for column in categories_df:
+        categories_df[column] = categories_df[column].map(lambda x: x.split('-')[1]).astype(int)
+    df.drop('categories', axis=1, inplace=True)
+    return df.merge(categories_df, how='left', left_index=True, right_index=True)
 
-
-def save_data(df, database_filename):
-    pass  
+def save_data(df, database_filename, if_exists):
+    table_name='messages'
+    engine = sqlalchemy.create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('messages', engine, if_exists=if_exists, index=True)
 
 
 def main():
-    if len(sys.argv) == 4:
+    if len(sys.argv) >= 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
@@ -24,11 +35,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+        print(df.head())
+        #
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-        
-        print('Cleaned data saved to database!')
+
+        if len(sys.argv) == 5:
+            if_exists=sys.argv[4]
+        else:
+            if_exists='replace'
+
+        save_data(df, database_filepath, if_exists)
+        #
+        # print('Cleaned data saved to database!')
     
     else:
         print('Please provide the filepaths of the messages and categories '\
